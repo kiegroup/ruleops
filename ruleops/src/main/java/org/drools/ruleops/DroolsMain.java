4ec8abb03ea4f4748e66ee210021008f7d34d578
Luca Molteni <volothamp@gmail.com>
2023-03-22 11:31:47 +0100

    First example of a SOP for RHOC (#24)
    * Run a specific KieBase with `-Druleops.kiebase=rhoc`
    * Upgrade to Drools 8.35.0.Final to support multi line consequences
    * Support for passing parameters to SOPs
    * Insert every pod inside the working memory
    * Change Debug listener to trace
    * Moved original RuleOps example to `basicRuleCheck` kiebase

--- ruleops/src/main/java/org/drools/ruleops/DroolsMain.java
+++ ruleops/src/main/java/org/drools/ruleops/DroolsMain.java
@@ -11,19 +11,27 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain(name = "cli")
 public class DroolsMain implements QuarkusApplication {
-  @Inject
-  DroolsSingleton drools;
 
-  @Override
-  public int run(String... args) throws Exception {
-    List<Advice> advices = drools.evaluateAllRulesStateless();
-    System.out.println("");
-    System.out.println("🤔 found "+advices.size()+" advices by looking into the k8s cluster:");
-    for (Advice a : advices) {
-      System.out.println("");
-      System.out.println("  💡 " + a.title() + " - " + a.description());
+    @Inject
+    DroolsSingleton drools;
+
+    @Override
+    public int run(String... args) throws Exception {
+        List<Advice> advices = drools.evaluateAllRulesStateless(args);
+
+        System.out.println("");
+        System.out.printf("  🤔 found %d advices by looking into the k8s cluster:", advices.size());
+        System.out.println("");
+
+        for (Advice a : advices) {
+            System.out.println("");
+            System.out.printf("  💡 %s%n", a.title());
+            System.out.println("");
+            System.out.println(a.description());
+        }
+
+        System.out.println("");
+
+        return 0;
     }
-    System.out.println("");
-    return 0;
-  }
 }
